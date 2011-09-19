061a0ee7b0eae73ef197a41b0b260d11b42bdeb6
Lin Zhang <myhere.2009@gmail.com>
2011-09-19 11:46:18 +0800

    add batch files

---
+++ proxy.bat
@@ -0,0 +1,1 @@
+plink -ssh -D 7070 -i D:/bin-in-path/putty/keys/pri.ppk -P 36001 myhere@zhangl.in
---
+++ vim.bat
@@ -0,0 +1,51 @@
+@echo off
+rem variable scope
+setlocal
+
+rem session 文件保存的位置: 用户目录下
+set SESSION_FILE=%HOMEDRIVE%%HOMEPATH%\vim-session.vim
+rem vim 程序所在位置
+set VIM_PROGRAM=%PROGRAMFILES%/Vim/vim73/vim.exe
+
+
+if "%1" == "" ( rem 无参数 -> 带 session 启动
+    if exist "%SESSION_FILE%" (
+        "%VIM_PROGRAM%" -S "%SESSION_FILE%"
+    ) else "%VIM_PROGRAM%"
+) else ( rem 有参数
+	set noSessionFile=true
+	set args=
+
+	rem 处理参数
+	:LoopStart
+		if "%1"=="" (
+			goto LoopEnd
+		) else (
+			if "%1" == "-s" (
+				set noSessionFile=false
+			) else (
+				set args=%args% %1
+			)
+			shift
+			goto LoopStart
+		)
+
+	:LoopEnd
+	if "%noSessionFile%"=="true" (
+		rem "无 -ns -> 不带 session 启动"
+        "%VIM_PROGRAM%" %args%
+	) else (
+		rem "有 -s -> 带 session 启动"
+        "%VIM_PROGRAM%" %args% -S "%SESSION_FILE%"
+	)
+)
+endlocal
+
+rem 使用说明:
+rem = vim 不加参数使用上次 session 启动
+rem = vim 加参数
+rem     = 参数中有 -s, 带上参数以上次 session 启动
+rem     = 参数中无 -s, 不加 session 启动
+rem
+rem
+rem vim -n (启动vim, 创建新文件)
