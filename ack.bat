390294ee9b9277ba7ebcf0b7f968e805e421740f
Lin Zhang <myhere.2009@gmail.com>
2012-07-17 00:35:06 +0800

    add ack-standalone.pl(depend on perl which is shipped with msysgit)

---
+++ ack.bat
@@ -0,0 +1,8 @@
+@echo off
+
+@perl %~dp0\ack-standalone.pl %*
+
+rem windows 下 来自 msysgit 
+rem ack 获取
+rem http://stackoverflow.com/questions/1023710/how-can-i-install-and-use-ack-library-on-windows
+rem http://betterthangrep.com/ack-standalone
