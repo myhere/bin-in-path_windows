84bbfb04221277d7555c304aabad9f88a3a0b4e2
Lin Zhang <myhere.2009@gmail.com>
2012-07-17 00:55:50 +0800

    update ack.bat

--- ack.bat
+++ ack.bat
@@ -2,7 +2,7 @@
 
 @perl %~dp0\ack-standalone.pl %*
 
-rem windows 下 来自 msysgit 
+rem windows 下 perl 来自 msysgit 
 rem ack 获取
 rem http://stackoverflow.com/questions/1023710/how-can-i-install-and-use-ack-library-on-windows
 rem http://betterthangrep.com/ack-standalone
